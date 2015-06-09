561c8efda381f5c1875ae1088eb7e1f270919560
unknown <kostiantyn.bogapov@KBP1-DNN-18132.synapse.com>
2015-06-09 17:36:56 +0300

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		uglify: {
+			build: {
+				src: ["js/scripts.js", "js/main.js"],
+				dest: "js/scripts.min.js"
+			}
+		},
+
+		less: {
+			development: {
+				options: {
+					/*compress: true,
+					yuicompress: true,
+					optimization: 2*/
+				},
+				files: {
+					"css/styles.css" : "less/main.less"
+				}
+			}
+		},
+
+
+
+		cssmin: {
+			with_banner: {
+				
+				options: {
+					banner: "/* Minified CSS */"
+				},
+
+				
+				files : {
+					'css/styles.min.css': ['css/styles.css']
+				}
+			}
+		},
+
+		watch: {
+			options: {
+				livereload: true
+			},
+			scripts: {
+				files : ["js/*.js"] ,
+				tasks : ["uglify"]
+			},
+
+			css: {
+				files : ["css/*.css"] ,
+				tasks : ["cssmin"]	
+			},
+
+			styles : {
+				files : ['less/**/*.less'],
+				tasks : ["less"],
+				options: {
+					nospawn : true
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-cssmin");
+	grunt.loadNpmTasks("grunt-contrib-less");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+
+	grunt.registerTask("default", ['uglify', 'less', 'cssmin', 'watch'])
+};
